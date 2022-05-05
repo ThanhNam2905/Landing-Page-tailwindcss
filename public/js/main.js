2cd98930532420336493f27e9cdefa16d8bf8171
nam <you@example.com>
2022-05-06 01:51:31 +0700

    Add: Complete UI Landing-Page

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,26 @@
 // navbar-menu
 const menuBtnEle = document.querySelector('#menu-btn i');
 const navbarMenuEle = document.querySelector('#navbar');
+const scrollTopBtnEle = document.querySelector('#scroll-top i');
 
 menuBtnEle.addEventListener('click', () => {
     navbarMenuEle.classList.toggle('active');
     menuBtnEle.classList.toggle('fa-xmark');
+    scrollTopBtnEle.classList.remove("active");
 })
+
+scrollTopBtnEle.addEventListener('click', () => {
+    scrollTo({
+        behavior: 'smooth',
+        top: 0
+    })
+})
+
+window.onscroll = () => {
+    if(scrollY > 300) {
+        scrollTopBtnEle.classList.add("active");
+    }
+    else {
+        scrollTopBtnEle.classList.remove("active");
+    }
+}
